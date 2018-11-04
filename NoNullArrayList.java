d5bf18e23acfb4d021e89682d288d57da3bda07f
Sophie Nichol <snichol00@stuy.edu>
2018-11-03 22:54:18 -0400

    first attempt, my computer unsaved

---
+++ NoNullArrayList.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+
+  public NoNullArrayList(int StartingCapacity){
+    super(StartingCapacity);
+  }
+
+  public boolean add(T str){
+    if (str == null){
+      throw new IllegalArgumentException();
+    }
+    else{
+      super.add(str);
+    }
+    return true;
+  }
+
+  public void add(int idx, T str){
+    if (str == null){
+      throw new IllegalArgumentException();
+    }
+    else{
+      super.add(idx, str);
+    }
+  }
+
+  public T set(int idx, T str){
+    T old = str;
+    if (str == null){
+      throw new IllegalArgumentException();
+    }
+    else{
+      super.set(idx, str);
+    }
+    return old;
+  }
+}
